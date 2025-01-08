d17abd450052b6509847fe2333bdb30fe3a5b906
Sanni-Singh <aryansingh7371@gmail.com>
2025-01-08 22:35:17 +0530

    chnges

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,7 @@
 const Loader = ()=>{
     return (
         <div className="flex flex-col gap-6 m-auto w-[700px] bg-slate-400">
+            <p className="text-center text-4xl font-bold">Ruko Abbhi tuhmre liya Hard question dhund raha hu</p>
             <div className=" w-[100%] h-[20px] bg-slate-200"></div>
             <div className=" w-[100%] h-[20px] bg-slate-200"></div>
             <div className=" w-[100%] h-[20px] bg-slate-200"></div>
